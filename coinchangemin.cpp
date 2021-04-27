38eb0012205514911e9fe9edf91a15ba51efb715
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-04-27 19:33:32 +0530

    Update coinchangemin.cpp

---
+++ coinchangemin.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int coinchange(int coin[],int sum,int n){
+    int t[n+1][sum+1];
+
+    for(int j=1;j<=sum+1;j++){
+        if(j%coin[0]==0) t[1][j]= j/coin[0];
+        else t[1][j]=INT_MAX-1;
+    }
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<sum+1;j++){
+            if(i==0) t[i][j]=INT_MAX-1;
+            else if(j==0) t[i][j]=0;
+            else if(coin[i-1]<=j){
+                t[i][j]=min(t[i][j-coin[i-1]]+1,
+                            t[i-1][j]);
+            }
+            else t[i][j]=t[i-1][j];
+        }
+    }
+    return t[n][sum];
+}
+
+int main()
+{
+    int coin[]={1,2,3};
+    //int val[]={1,2,3,4,5};
+    int n = sizeof(coin)/sizeof(coin[0]);
+    int SUM1=5;
+    cout << "Max Profit:-" << coinchange(coin,SUM1,n);
+    return 0;
+}
